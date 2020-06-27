54aa631cf90fa746e388989fce1e7424743d7bf6
charconstpointer <attwistingnether@gmail.com>
2020-06-27 15:59:20 +0200

    client

---
+++ Rooster.Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Rooster.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5000/rooster")
+                .Build();
+
+            connection.On<dynamic>("onChanged", (message) =>
+            {
+                Console.WriteLine(message.Message);
+            });
+            connection.Closed += async error =>
+            {
+                await Task.Delay(new Random().Next(0,5) * 1000);
+                await connection.StartAsync();
+            };
+        }
+    }
+}
